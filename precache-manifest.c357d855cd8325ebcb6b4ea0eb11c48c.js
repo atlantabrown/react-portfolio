3093a5dce13e90e48d5ab34463af43a2b52ad6e2
AtlantaBrown <Atlanta.Brown@target.com>
2020-07-08 01:10:29 -0500

    Updates

---
+++ precache-manifest.c357d855cd8325ebcb6b4ea0eb11c48c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b958cd50286191579a5b534875e4c84d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2301a7d87acc3a0707cd",
+    "url": "/react-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cdcff9cf64b9cfeb6980",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2301a7d87acc3a0707cd",
+    "url": "/react-portfolio/static/js/2.0522f60f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.0522f60f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdcff9cf64b9cfeb6980",
+    "url": "/react-portfolio/static/js/main.5b637513.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  }
+]);
